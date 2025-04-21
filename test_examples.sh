e003c157fbab2ce5573909198de43383e8871e1e
lenkan <dlenksjo@gmail.com>
2025-04-21 11:43:12 +0100

    move to current dir at start

--- test_examples.sh
+++ test_examples.sh
@@ -2,17 +2,19 @@
 set -e
 
 output=$(npm pack)
+current_dir=$(pwd)
 
 for example in examples/*; do
     if [ -d "$example" ]; then
+        cd "$current_dir"
+
         echo "## Running tests in $example"
         mkdir -p "$example/cesr"
         tar -xf "$output" --dir "$example/cesr"
+
         cd "$example"
         image=$(docker build -q .)
         docker run --rm "$image"
         echo "## Success tests in $example"
-
-        cd "../../"
     fi
 done
