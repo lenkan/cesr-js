03c8db1993b222b8a7585e70c8338b67995df3bb
lenkan <dlenksjo@gmail.com>
2025-04-21 11:41:12 +0100

    run example tests in docker

--- test_examples.sh
+++ test_examples.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 set -e
 
+output=$(npm pack)
+
 for example in examples/*; do
     if [ -d "$example" ]; then
-        echo "Running tests in $example"
-        (cd "$example" && ./run_test.sh)
+        echo "## Running tests in $example"
+        mkdir -p "$example/cesr"
+        tar -xf "$output" --dir "$example/cesr"
+        cd "$example"
+        image=$(docker build -q .)
+        docker run --rm "$image"
+        echo "## Success tests in $example"
+
+        cd "../../"
     fi
 done
